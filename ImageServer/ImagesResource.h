be8f97a0a80c2308030bbe15b59b3e54bddefd28
harshakush <harsha.kushtagi@gmail.com>
2015-02-18 23:00:38 +0530

    More changes

--- ImageServer/ImagesResource.h
+++ ImageServer/ImagesResource.h
@@ -6,7 +6,21 @@
 using namespace std;
 class ImagesResource : public RestInterface {
 
+public:
+	virtual ServerResponsePtr get(const ServerRequestPtr request){
+		ServerResponsePtr response = ServerResponsePtr(new ServerResponse());
+		response->setResponse("simple response");
 
+	}
+	virtual ServerResponsePtr post(const ServerRequestPtr request) {
+
+	}
+	virtual ServerResponsePtr put(const ServerRequestPtr request) {
+
+	}
+	virtual ServerResponsePtr del(const ServerRequestPtr request) {
+
+	}
 };
 
 #endif
--- ImageServer/ServerResponse.h
+++ ImageServer/ServerResponse.h
@@ -3,15 +3,22 @@
 #define SERVER_RESPONSE_HEADER
 
 #include <memory>
+#include <string>
 using namespace std;
 
 class ServerResponse {
 public:
 	ServerResponse() {
-
+	}
+	void setResponse(string response) {
+		m_response = response;
 	}
 
-
+	string getResponse() {
+		return m_response;
+	}
+private :
+	string m_response;
 };
 typedef shared_ptr<ServerResponse> ServerResponsePtr;
 
