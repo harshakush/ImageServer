581bd8842c0cb918194aafaa2e28f9e13445298a
harshakush <harsha.kushtagi@gmail.com>
2015-02-26 23:17:25 +0530

    Json processing code
    ServerRequest now supports retrieval of json object. More refactoring
    will be done.

--- ImageServer/ServerRequest.h
+++ ImageServer/ServerRequest.h
@@ -6,31 +6,50 @@
 #include <cpprest\http_msg.h>
 using namespace std;
 using namespace web::http;
+
 class ServerRequest {
-public: ServerRequest(http_request message) : m_requestMessage(message) {
-			
-		}
-		const http_request& getRequest() {
-			return m_requestMessage;
-		}
-		string getUri() {
-			return ::utility::conversions::to_utf8string(m_requestMessage.request_uri().to_string());
-		}
-		ServerDataTypes::rest_methods getMethod() {
 
-			return ServerDataTypes::rest_methods::IGET;
-		}
+public: 
+	ServerRequest(http_request message) : m_requestMessage(message) {
+		init();
+	}
 
-		web::json::value getJson() {
-			m_jsonTask = m_requestMessage.extract_json();
-			
-			m_jsonRequest = m_jsonTask.get();
-			return m_jsonRequest;
-		}
+	const http_request& getRequest() {
+		return m_requestMessage;
+	}
+
+	string getUri() {
+		return ::utility::conversions::to_utf8string(m_requestMessage.request_uri().to_string());
+	}
+
+	ServerDataTypes::rest_methods getMethod() {
+
+		return ServerDataTypes::rest_methods::IGET;
+	}
+
+	web::json::value getJson() {
+		return m_jsonRequest;
+	}
 private:
+	void init() {
+		web::json::value answer;
+		m_requestMessage.extract_json().then([&answer](pplx::task<web::json::value> task) {
+			try {
+				answer = task.get();
+			}
+			catch (http_exception const & e)
+			{
+				wcout << e.what() << endl;
+			}
+		})
+			.wait();
+		//this wait here makes it synchronous
+		m_jsonRequest = answer;
+	}
+
 	const http_request m_requestMessage;
 	web::json::value m_jsonRequest;
-	pplx::task<web::json::value> m_jsonTask;
 };
+
 typedef shared_ptr<ServerRequest> ServerRequestPtr;
 #endif
